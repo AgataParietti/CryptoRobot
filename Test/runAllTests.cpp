7b78b6a4ce50a7e75dd89c897171b590a970480a
AgataParietti <agata.parietti@stud.unifi.it>
2019-07-05 15:42:32 +0200

    First Commit

---
+++ Test/runAllTests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Agata Parietti on 2019-07-05.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    sf::Font font;
+    font.loadFromFile("Arial.ttf");
+    if (!font.loadFromFile("arial.ttf")) {
+        std::cout<<"ERROR!"<<std::endl;
+    }
+
+
+    sf::Text text;
+    text.setString("HELLO");
+    text.setCharacterSize(30);
+    text.setFont(font);
+    text.setFillColor(sf::Color::Black);
+
+
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while( window.pollEvent(event)) {
+            if(event.type==sf::Event::Closed)
+                window.close();
+
+            window.clear(sf::Color::Yellow);
+            window.draw(text);
+            window.display();
+        }
+    }
+
+    //return 0;
+}
